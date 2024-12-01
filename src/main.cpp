aeae26f2412e0becd0b76bb05b893203d495aefa
Anh Nguyen <94160753+AnhNguyenlost13@users.noreply.github.com>
2024-12-01 20:41:40 +0700

    hi melly again

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,11 @@ class $modify(whatTheHellLmao, GJBaseGameLayer) {
 		GJBaseGameLayer::gameEventTriggered(mrrrp, meow, mrrow);
 		// log::debug("Game event triggered. {}, {}, {}", static_cast<int>(mrrrp), meow, mrrow);
 		if (static_cast<int>(mrrrp) == 10) {
-			GJBaseGameLayer::updateScreenRotation(180, true, false, 0.5, 1, 2, 4082, 0);
 			GJBaseGameLayer::toggleFlipped(true, false);
+			GJBaseGameLayer::updateScreenRotation(180, true, false, 0.5, 1, 2, 4082, 0);
 		} else if (static_cast<int>(mrrrp) == 11) {
-			GJBaseGameLayer::updateScreenRotation(0, false, true, 0.5, 1, 2, 4082, 0);
 			GJBaseGameLayer::toggleFlipped(false, false);
+			GJBaseGameLayer::updateScreenRotation(180, true, false, 0.5, 1, 2, 4082, 0);
 		}
 	}
 };
