9df06cd5ca6e736fc245c7df256c6ce461a40905
Anh Nguyen <94160753+AnhNguyenlost13@users.noreply.github.com>
2024-12-01 17:47:07 +0700

    nuh uh

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@ using namespace geode::prelude;
 
 #include <Geode/modify/GJBaseGameLayer.hpp>
 class $modify(whatTheHellLmao, GJBaseGameLayer) {
-	void gameEventTriggered(GJGameEvent me, int la, int nie) {
-		GJBaseGameLayer::gameEventTriggered(me, la, nie);
-		log::debug("Game event triggered. {}, {}, {}", static_cast<int>(me), la, nie);
-		if (static_cast<int>(me) == 10) {
+	void gameEventTriggered(GJGameEvent mrrrp, int meow, int mrrow) {
+		GJBaseGameLayer::gameEventTriggered(mrrrp, meow, mrrow);
+		// log::debug("Game event triggered. {}, {}, {}", static_cast<int>(mrrrp), meow, mrrow);
+		if (static_cast<int>(mrrrp) == 10) {
 			GJBaseGameLayer::updateScreenRotation(180, true, false, 0.5, 1, 2, 4082, 0);
 			GJBaseGameLayer::toggleFlipped(true, false);
-		} else if (static_cast<int>(me) == 11) {
+		} else if (static_cast<int>(mrrrp) == 11) {
 			GJBaseGameLayer::updateScreenRotation(0, false, true, 0.5, 1, 2, 4082, 0);
 			GJBaseGameLayer::toggleFlipped(false, false);
 		}
