a1a08ae432a587586c4b382e603d57fc09f91a51
Nya Meow <nyameowmeow@icloud.com>
2024-12-02 17:40:59 +0700

    turning thesillydoggo

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,13 @@ class $modify(whatTheHellLmao, GJBaseGameLayer) {
 		GJBaseGameLayer::gameEventTriggered(mrrrp, meow, mrrow);
 		// log::debug("Game event triggered. {}, {}, {}", static_cast<int>(mrrrp), meow, mrrow);
 		if (static_cast<int>(mrrrp) == 10) {
+			log::info("yes");
 			GJBaseGameLayer::toggleFlipped(true, false);
-			GJBaseGameLayer::updateScreenRotation(180, true, false, 0.5, 1, 2, 4082, 0);
+			GJBaseGameLayer::updateScreenRotation(90, true, false, 0.5, 1, 2, 9999, 0);
 		} else if (static_cast<int>(mrrrp) == 11) {
+			log::info("no");
 			GJBaseGameLayer::toggleFlipped(false, false);
-			GJBaseGameLayer::updateScreenRotation(180, true, false, 0.5, 1, 2, 4082, 0);
+			GJBaseGameLayer::updateScreenRotation(90, true, false, 0.5, 1, 2, 9999, 0);
 		}
 	}
 };
